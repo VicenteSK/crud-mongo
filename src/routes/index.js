aa0ef7009cb642739208719d3347ac17d7609ccd
Vicente Valenzuela <vicente.valenzuela.cortes@ciisa.cl>
2021-10-14 17:06:05 -0300

    carga inicial

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const Task = require('../models/task');
+
+router.get('/', (req, res) => {
+    res.render('index');
+}); 
+
+router.post('/add', async (req, res) => {
+    const Task = new Task(req.body);
+    await task.save();
+    res.send('received');
+});  
+
+module.exports = router;
